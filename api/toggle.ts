c5ed52a01e40b405da1708644b959698b96a2866
zackkrida <zackkrida@protonmail.com>
2020-03-04 13:29:43 -0500

    📦 NEW: -m Finished :)

--- api/toggle.ts
+++ api/toggle.ts
@@ -1,18 +1,23 @@
-import { NowRequest, NowResponse } from "@now/node"
-import fetch from "isomorphic-unfetch"
-import crypto from "crypto"
+import { NowRequest, NowResponse } from '@now/node'
+import fetch from 'isomorphic-unfetch'
+import crypto from 'crypto'
 
 const token = process.env.TODOIST_API_KEY
+const themes = {
+  dark: 11,
+  light: 0
+}
 
 export default async (req: NowRequest, res: NowResponse) => {
+  const theme = (req.query.theme as string) || 'dark'
+
   const todoist = await todoistAPI({
-    path: "",
     data: {
       commands: [
         {
-          type: "user_update",
-          uuid: crypto.randomBytes(20).toString("hex"),
-          args: { theme: 11 }
+          type: 'user_update',
+          uuid: crypto.randomBytes(20).toString('hex'),
+          args: { theme: themes[theme] }
         }
       ]
     }
@@ -21,17 +26,20 @@ export default async (req: NowRequest, res: NowResponse) => {
   res.json(todoist)
 }
 
-const todoistAPI = async ({ path = "", data = {}, ...options }) => {
-  let res = await fetch(`https://todoist.com/api-route${path}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      token,
-      ...data
+const todoistAPI = async ({ data = {}, ...options }) => {
+  try {
+    let res = await fetch(`https://api.todoist.com/sync/v8/sync`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        token,
+        ...data
+      })
     })
-  })
-
-  return await res.json()
+    return await res.json()
+  } catch (error) {
+    console.error(error.message)
+  }
 }
